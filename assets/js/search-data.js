e3aea57017d7d6ca410365f916e102733f4b3701
GDW190808 <55833356+GDW190808@users.noreply.github.com>
2026-04-17 02:28:54 +0000

    Deploying to gh-pages from @ GDW190808/GDW190808.github.io@09b8621c65bb37b6b474b57eb2ca30404090798d 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -421,9 +421,9 @@ ninja.data = [{
           description: "zl4",
           section: "Projects",handler: () => {
               window.location.href = "/projects/4_project/";
-            },},{id: "projects-project-5",
-          title: 'project 5',
-          description: "a project with a background image",
+            },},{id: "projects-一种基于分层原理的云锦风格图像生成方法",
+          title: '一种基于分层原理的云锦风格图像生成方法',
+          description: "zl5",
           section: "Projects",handler: () => {
               window.location.href = "/projects/5_project/";
             },},{id: "projects-project-6",
