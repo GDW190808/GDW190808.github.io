91ed6a460fa4904c687cab09527caf42d27ae8bf
GDW190808 <55833356+GDW190808@users.noreply.github.com>
2026-04-17 02:35:59 +0000

    Deploying to gh-pages from @ GDW190808/GDW190808.github.io@74b3d794f3108cc8d8ae433933392e7856b341b3 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -426,9 +426,9 @@ ninja.data = [{
           description: "zl5",
           section: "Projects",handler: () => {
               window.location.href = "/projects/5_project/";
-            },},{id: "projects-project-6",
-          title: 'project 6',
-          description: "a project with no image",
+            },},{id: "projects-一种仿云锦妆花缎的印花面料及其制备方法",
+          title: '一种仿云锦妆花缎的印花面料及其制备方法',
+          description: "zl6",
           section: "Projects",handler: () => {
               window.location.href = "/projects/6_project/";
             },},{id: "projects-project-7",
