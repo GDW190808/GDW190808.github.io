74d2e24c0fc2811832e9e4c4501faf177dc64649
GDW190808 <55833356+GDW190808@users.noreply.github.com>
2026-04-18 14:23:59 +0000

    Deploying to gh-pages from @ GDW190808/GDW190808.github.io@33be5a004828aea319f44be132b113514026995a 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -88,7 +88,7 @@ ninja.data = [{
               window.location.href = "/projects/4_project/";
             },},{id: "projects-一种基于分层原理的云锦风格图像生成方法",
           title: '一种基于分层原理的云锦风格图像生成方法',
-          description: "zl5",
+          description: "",
           section: "Projects",handler: () => {
               window.location.href = "/projects/5_project/";
             },},{id: "projects-一种仿云锦妆花缎的印花面料及其制备方法",
